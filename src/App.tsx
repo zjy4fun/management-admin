7752261f2be6bc7fd56ec42fb838c394fcb86ba6
zjy4fun <zjy4fun@gmail.com>
2023-01-15 01:41:12 +0800

    chore: add router todo

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,16 @@ import Login from "./pages/Login";
 function App() {
     return (
         <AuthProvider>
+            {/*此处应该有 Consumer，用来消费传进来的值或方法，根据传进来的值判断路由*/}
             <Routes>
                 <Route path="/login" element={<Login/>}/>
                 <Route element={<Layout/>}>
                     <Route path="/" element={<div>PublicPage</div>}/>
-                    <Route path="/protected" element={<div>protected</div>}/>
+                    {/*TODO*/}
+                    {/*<RequireAuth> 里面对UserContext做判断 */}
+                    {/*<RequireAuth>*/}
+                        <Route path="/protected" element={<div>protected</div>}/>
+                    {/*</RequireAuth>*/}
                 </Route>
             </Routes>
         </AuthProvider>
